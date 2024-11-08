f9cf34064af9fee18d6b6734b5048c68004ca4bd
Pascal Jahan Elahi <pascaljelahi@gmail.com>
2024-11-08 16:32:14 +0800

    fix for interactive notebooks in jupyterhub

--- lessons/useful_wrappers.py
+++ lessons/useful_wrappers.py
@@ -3,7 +3,14 @@
 # generic plotting library 
 import matplotlib
 import matplotlib.pyplot as plt
+import mpl_interactions.ipyplot as iplt
 matplotlib.logging.getLogger('matplotlib.font_manager').disabled = True
+# import interactive widgets
+from ipywidgets import interact, interactive, fixed, interact_manual
+import ipywidgets as widgets
+from IPython.display import display, Markdown, Latex
+
+import plotly.graph_objects as go
 
 # penny lane quantum computing library
 import pennylane as qml
@@ -14,27 +21,27 @@ qml.drawer.use_style('sketch')
 import qmuvi
 from qiskit import QuantumCircuit
 
-# import interactive widgets
-from ipywidgets import interact, interactive, fixed, interact_manual
-import ipywidgets as widgets
-from IPython.display import display, Markdown, Latex
+hfont = {'fontname':'Helvetica'}
 
 def PlotPennyLaneHisto(results, plottitle : str = ''):
     with plt.xkcd():
     # Based on "Stove Ownership" from XKCD by Randall Munroe
     # https://xkcd.com/418/
 
-        fig = plt.figure()
-        ax = fig.add_axes((0.1, 0.2, 0.8, 0.7))
+        # fig, ax = plt.subplots()
+        # plt.tight_layout()
+        plt.ioff()
+        fig = plt.figure(figsize=(7.5,7.5))
+        ax = fig.add_axes((0.2, 0.2, 0.5, 0.5))
         ax.spines[['top', 'right']].set_visible(False)
         labels = [rf'$|{k}>$' for k in results.keys()]
         xvals = np.arange(len(results.keys()))
         yvals = [results[k] for k in results.keys()]
         ax.bar(labels, yvals, facecolor='DarkOrange', edgecolor='Gold', linewidth=4)
         ax.tick_params(axis='x', labelrotation=80)
-        ax.set_xlabel('Qubit States')
-        ax.set_ylabel('Count')
-        ax.set_title(plottitle)
+        ax.set_xlabel('Qubit States', **hfont)
+        ax.set_ylabel('Count', **hfont)
+        ax.set_title(plottitle, **hfont)
         fig.show()
 
 def _setupqubits(num_qubits, add_H, hqubits, add_CNOT, cnotqubits):
@@ -98,9 +105,9 @@ def PlotSystemRequirements(num_qubits : int = 2, num_gates : int = 1, num_measur
             #'1 year on a laptop': 5e12/128*0.5*3.15e7,
             '1000 years on a laptop': 5e12/128*0.5*3.15e10,
              '1 second on a Supercomputer node':5e12,
-             '1 second on Setonix supercomputer':42e15,
+             '1 second on Setonix \nsupercomputer':42e15,
              '1 second on Frontier supercomputer or \n 2 years on a laptop':1e18,
-             '1 second on all Google cloud computing': 3.98 * 1e21,
+             '1 second on all Google \ncloud computing': 3.98 * 1e21,
 #             '1 second on Earth\'s computers':1e23,
 #             '1 year using Earth\'s computers':3e30,
         }
@@ -112,54 +119,61 @@ def PlotSystemRequirements(num_qubits : int = 2, num_gates : int = 1, num_measur
         display(Markdown(f'* Operations: This would require ${np.exp(flops):.4f}$ Floating Point Operations, taking roughly {time:.4f} seconds on a laptop'))
         maxlognqubit=9
         maxgates = 10
+        plt.ioff()
+        plt.tight_layout()
         x = 2**np.arange(maxlognqubit)
-        fig = plt.figure()
-        ax = fig.add_axes((0.1, 0.1, 0.9, 0.9))
-        ax.plot(x, memcalc(x, num_gates), linewidth = 4, zorder = 2, color='cornflowerblue', marker='None')
-        ax.scatter([num_qubits], [memcalc(num_qubits, num_gates)], 
+        fig, (ax1, ax2) = plt.subplots(2,1, figsize=(10,12))
+        plt.subplots_adjust(left=0.4, right=0.95, top=0.9, bottom=0.1, hspace=0.4)
+        # ax = fig.add_axes((0.1, 0.1, 0.9, 0.9))
+        ax1.plot(x, memcalc(x, num_gates), linewidth = 4, zorder = 2, color='cornflowerblue', marker='None')
+        ax1.scatter([num_qubits], [memcalc(num_qubits, num_gates)], 
                    zorder = 3, facecolor='Navy', edgecolor='LightBlue', marker='o', s=100)
-        ax.plot([0,num_qubits], [mem, mem], 
+        ax1.plot([0,num_qubits], [mem, mem], 
                 linewidth = 2, zorder = 1, color='lightblue', marker='None', linestyle='dashed')
-        ax.plot([num_qubits,num_qubits], [-2, mem], 
+        ax1.plot([num_qubits,num_qubits], [-2, mem], 
                 linewidth = 2, zorder = 1, color='lightblue', marker='None', linestyle='dashed')
-        ax.set_xscale('log')
-        ax.set_xlim([0,2**(maxlognqubit+0.1)])
-        ax.set_ylim([-2, np.max([mem*1.1, (memlist['All the storage on Earth'])])])
+        ax1.set_xscale('log')
+        ax1.set_xlim([0,2**(maxlognqubit+0.1)])
+        ax1.set_ylim([-2, np.max([mem*1.1, (memlist['All the storage on Earth'])])])
 
-        ax.set_yticks([memlist[k] for k in memlist.keys()], 
+        ax1.set_yticks([memlist[k] for k in memlist.keys()], 
                       labels=memlist.keys()) 
-        ax.set_xticks([1, 2, 4, 8, 16, 32, 64, 128, 256],
+        ax1.set_xticks([1, 2, 4, 8, 16, 32, 64, 128, 256],
                       labels=['1', '2', '4', '8', '16', '32', '64', '128', '256']
                      ) 
-        ax.set_xlabel('Number of Qubits')
-        ax.set_ylabel('Amount of Memory')
-        ax.set_title('How much does it take to simulate on a classical computer?')
-        fig.show()
+        ax1.set_xlabel('Number of Qubits')
+        #ax1.set_ylabel('Amount of Memory')
+        ax1.set_title('How much does memory does it take to \nsimulate on a classical computer?')
+        # fig.show()
 
 
-        fig = plt.figure()
-        ax = fig.add_axes((0.1, 0.1, 0.9, 0.9))
-        ax.plot(x, flopcalc(x, num_gates, num_measurements), linewidth = 4, zorder = 2, color='darkorange', marker='None')
-        ax.scatter([num_qubits], [flopcalc(num_qubits, num_gates, num_measurements)], 
+        # fig2 = plt.figure()
+        # ax2 = fig.add_axes((0.1, 0.1, 0.9, 0.9))
+        ax2.plot(x, flopcalc(x, num_gates, num_measurements), linewidth = 4, zorder = 2, color='darkorange', marker='None')
+        ax2.scatter([num_qubits], [flopcalc(num_qubits, num_gates, num_measurements)], 
                    zorder = 3, facecolor='darkgoldenrod', edgecolor='gold', marker='o', s=100)
         
-        ax.plot([0,num_qubits], [flops, flops], 
+        ax2.plot([0,num_qubits], [flops, flops], 
                 linewidth = 2, zorder = 1, color='gold', marker='None', linestyle='dashed')
-        ax.plot([num_qubits, num_qubits], [-10, flops], 
+        ax2.plot([num_qubits, num_qubits], [-10, flops], 
                 linewidth = 2, zorder = 1, color='gold', marker='None', linestyle='dashed')
-        ax.set_xscale('log')
-        ax.set_yticks([np.log(flopslist[k]) for k in flopslist.keys()], 
+        ax2.set_xscale('log')
+        ax2.set_yticks([np.log(flopslist[k]) for k in flopslist.keys()], 
                       labels=flopslist.keys())
-        ax.set_xticks([1, 2, 4, 8, 16, 32, 64, 128, 256],
+        ax2.set_xticks([1, 2, 4, 8, 16, 32, 64, 128, 256],
                       labels=['1', '2', '4', '8', '16', '32', '64', '128', '256']
                      ) 
-        ax.set_xlim([0,2**(maxlognqubit+0.1)])
-        ax.set_ylim([-10, np.max([flops*1.1, np.log(flopslist[list(flopslist.keys())[-1]])])])
+        ax2.set_xlim([0,2**(maxlognqubit+0.1)])
+        ax2.set_ylim([-10, np.max([flops*1.1, np.log(flopslist[list(flopslist.keys())[-1]])])])
 
-        ax.set_xlabel('Number of Qubits')
-        ax.set_ylabel('Amount of Time')
-        ax.set_title('How long does it take to simulate on a classical computer?')
+        ax2.set_xlabel('Number of Qubits')
+        #ax2.set_ylabel('Amount of Time')
+        ax2.set_title('How long does it take to simulate \non a classical computer?')
+        ax1.tick_params(axis='y', labelrotation=-5)
+        ax2.tick_params(axis='y', labelrotation=-5)
+
         fig.show()
+
 
 def MakeCircuit(num_qubits : int = 1, 
                 add_H : bool = True, 
@@ -186,6 +200,7 @@ def MakeCircuit(num_qubits : int = 1,
 
     _reportsim(num_qubits, num_gates, report_system_requirements)
     # plotting circuit
+    plt.ioff()
     fig, ax = qml.draw_mpl(circuit, show_all_wires=True)(qubits, hqubits, cnotqubits)
     fig.show()
     
@@ -218,6 +233,7 @@ def MakeAndRunCircuit(num_measurements :int = 100,
 
     _reportsim(num_qubits, num_gates, report_system_requirements)
     # plotting circuit
+    plt.ioff()
     fig, ax = qml.draw_mpl(circuit, show_all_wires=True)(qubits, hqubits, cnotqubits)
     fig.show()
     if (num_measurements > max_nshots) or (num_qubits > max_nqubits):
